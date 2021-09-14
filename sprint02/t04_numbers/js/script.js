d53553f2a38c605fd2e205420029b885e2512f5e
asamson <Freefire25>
2021-09-14 21:10:31 +0300

    t04 done / t05

--- sprint02/t04_numbers/js/script.js
+++ sprint02/t04_numbers/js/script.js
@@ -1,19 +1,38 @@
-let num1 = +prompt("Input beginRange from 1-100");
-let num2 = +prompt("Input endRange from 1-100");
-
-
-if (isNaN(num1) || isNaN(num2)) {
-    alert('only number from 1-100')
-}  else if (1 > num1 || num1 >= 101) {
-    alert(`Your input beginRange ${num1} != range from 1-100`)
-} else if (1 > num2 || num2 >= 101) {
-    alert(`Your input endRange ${num2} != range from 1-100`)
-} else if (num2 < num1) {
-    alert('endRange cat\'t be more then beginRange')
-}
 
 function checkDivision(beginRage, endRage) {
+    beginRage = +prompt("Input beginRange from 1-100");
+    endRage = +prompt("Input endRange from 1-100");
 
+    if (isNaN(beginRage) || isNaN(endRage)) {
+        alert('only number from 1-100')
+        checkDivision();
+    } else if (1 > beginRage || beginRage >= 101) {
+        alert(`Your input beginRange ${beginRage} != range from 1-100`)
+        checkDivision();
+    } else if (1 > endRage || endRage >= 101) {
+        alert(`Your input endRange ${endRage} != range from 1-100`)
+        checkDivision();
+    } else if (endRage < beginRage) {
+    alert('endRange cat\'t be more then beginRange')
+        checkDivision();
+    }
+
+    while (beginRage < endRage + 1) {
+        if (beginRage % 2 === 0 && beginRage % 3 === 0 && beginRage % 10 === 0) {
+            console.log(`${beginRage} is even, a multiple of 3, a multiple of 10`)
+        } else if (beginRage % 2 === 0 && beginRage % 3 === 0) {
+            console.log(`${beginRage} is even,is a multiple of 3`)
+        } else if (beginRage % 2 === 0 && beginRage % 10 === 0) {
+            console.log(`${beginRage} is even,a multiple of 10`)
+        } else if (beginRage % 3 === 0) {
+            console.log(`${beginRage} is a multiple of 3`)
+        } else if (beginRage % 2 === 0) {
+            console.log(`${beginRage} is even`)
+        } else {
+            console.log(`${beginRage} - `)
+        }
+        beginRage +=1;
+    }
 }
 
-console.log(num1,num2);
+checkDivision();
