333fe8237465b0ec5dd8a46608c024d40bdf4b26
asamson <Freefire25>
2021-09-29 21:49:14 +0300

    part

---
+++ sprint02/t08_display_heroes/js/script.js
@@ -0,0 +1,13 @@
+function displayHeroes(team, universe, race, eyeColor, hairColor) {
+    if (team === "Avengers" || team === "S.H.I.E.L.D" && universe === "Marvel" && race === "human"
+        && eyeColor === "green" && hairColor === "lightBrown/green")
+        return alert("This is Black Widow!");
+    else if (team === "Justice League Of America" || team === "Teen Titans" && universe === "DC Comics"
+        && race === "human" || race === "kryptonian" && eyeColor === "blue" && hairColor === "black")
+        return alert("This is a Superman or Robin!");
+    else alert("Didn't recognize!");
+}
+
+// displayHeroes("S.H.I.E.L.D", "Marvel", "human", "green", "lightBrown/green");
+// displayHeroes("Justice League Of America", "DC Comics", "human", "blue", "black");
+// displayHeroes("Teen Titans", "DC Comics", "kryptonian", "blue", "black");
---
+++ sprint02/t09_namemaker/js/script.js
@@ -0,0 +1,57 @@
+'use strict';
+
+let regexName = /^[a-zA-Z]+$/;
+let regexGender = /^(male|female|)$/i;
+let regexAge = /^[1-9]{1,5}$/;
+
+
+function validationInput() {
+    let hero = {};
+
+    hero.name = prompt("What animal is the superhero most similar to?");
+    if (hero.name.length >= 20) {
+        alert("Maximum length is 20 symbols");
+        validationInput();
+    } else if (!regexName.test(hero.name)) {
+        alert("only letters!");
+        validationInput();
+    }
+
+    hero.gender = prompt("Is the superhero male or female? Leave blank if unknown or other.");
+    if (regexGender.exec(hero.gender) === null) {
+        alert('Error input, try again gender');
+        validationInput();
+    }
+
+    hero.age = prompt("How old is the superhero?");
+    if (hero.age.length >= 5) {
+        alert("Maximum length is 4 symbols for age");
+        validationInput();
+    } else if (!regexAge.exec(hero.age)) {
+        alert('Error input, try again');
+        validationInput();
+    }
+    alert(`The superhero name is:${hero.name}-${description(hero)}`)
+}
+
+function description(hero) {
+    if (hero.age < 18) {
+        if (hero.gender === 'male') {
+            return 'boy';
+        } else if (hero.gender === 'female') {
+            return 'girl';
+        } else {
+            return 'kid';
+        }
+    } else {
+        if (hero.gender === 'male') {
+            return 'man';
+        } else if (hero.gender === 'female') {
+            return 'woman';
+        } else {
+            return 'hero';
+        }
+    }
+}
+
+validationInput();
