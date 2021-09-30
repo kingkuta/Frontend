6f2cec3f881bf5678a071d9537069283f415639c
asamson <Freefire25>
2021-10-01 00:02:26 +0300

    new

--- sprint02/t12_heroes/js/script.js
+++ sprint02/t12_heroes/js/script.js
@@ -1,4 +1,34 @@
+'use strict'
+
+
+var heroes = ["Captain America", "Hulk", "Thor", "Iron Man", "Spider-Man"];
+console.log(heroes)
+
 function firstElements(arr, n) {
+    let newArr = [];
+
+    for (let i = 0; i <= n - 1; i++) {
+        newArr[i] = arr[i];
+    }
+    if (n > arr.length) {
+        return newArr.splice(arr.length,n)
+    }
+
 
 }
 
+
+// console.log(firstElements(heroes, 5));
+// // // ["Captain America", "Hulk", "Thor", "Iron Man", "Spider-Man"]
+// //
+// console.log(firstElements(heroes, 1));
+// // // ["Captain America"]
+// //
+// console.log(firstElements(heroes, 3));
+// // // ["Captain America", "Hulk", "Thor"]
+//
+console.log(firstElements(heroes, 8));
+// // ["Captain America", "Hulk", "Thor", "Iron Man", "Spider-Man"]
+//
+// console.log(firstElements(heroes, -1));
+// // // []
