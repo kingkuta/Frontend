5f48f189d5798dbf559d8b19984f658a9f8284ae
asamson <Freefire25>
2021-10-10 02:46:55 +0300

    first commit

---
+++ ustore/js/lang.js
@@ -0,0 +1,31 @@
+const langArr = {
+    "unit": {
+        "ru": "создаем магазин uStore",
+        "en": "build a uStore shop",
+        "ua": "створюємо магазин uStore",
+    },
+    "catalog": {
+        "ru": "Каталог",
+        "en": "Catalog",
+        "ua": "Каталог",
+    },
+
+    "howToBuy": {
+        "ru": "Как купить",
+        "en": "How To buy",
+        "ua": "Як придбати",
+    },
+
+    "about": {
+        "ru": "Про нас",
+        "en": "About",
+        "ua": "Про нас",
+    },
+    "contacts": {
+        "ru": "Контакты",
+        "en": "Contacts",
+        "ua": "Контакти",
+    },
+}
+
+
